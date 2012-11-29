d8dba18bd129de6ad1675a25c7bb0f5cfd2a64b9
bharatsamurai <bharat.cusat@gmail.com>
2012-11-29 15:47:06 +0530

    Update call.js

--- call.js
+++ call.js
@@ -1,19 +1,16 @@
 var p = require('./plivo');
 var p1 =  p.Plivo();
 
-p1.options.authId = "MAM2JMMZI0ZDE0Y2U2MZ";
-p1.options.authToken = "ZDVlZTU0ZGEzZmI3NzAwODU1NDllMmJmNmJlNmJj";
-
-console.log(p1.options.host);
+p1.options.authId = "xxxxxx";
+p1.options.authToken = "xxxxxx";
 
 var params = {};
-params.from = "918050420391";
-params.to = "917411201001";
-params.answer_url = "http://dl.dropbox.com/u/109700465/answer_url.xml";
+params.from = "xxxxx";
+params.to = "xxxxx";
+params.answer_url = "http://somesite.com/answer_url.xml";
 
-console.log(params);
 var json = JSON.stringify(params);
-console.log(json);
+
 p1.make_call(params,function callback(err, response) {
     console.log(response.statusCode,response);
     console.log(err);
